15007bfd3d928894a4f62c5e986eaac23c4ac8fe
tmoriics <tmoriics-tky@umin.ac.jp>
2022-08-06 20:56:59 +0900

    Updated colors.

--- streamlit_setup.sh
+++ streamlit_setup.sh
@@ -11,8 +11,8 @@ enableCORS = false
 
 [theme]
 primaryColor='#F63366'
-backgroundColor='#88EE558'
-secondaryBackgroundColor='#F0F2F6'
+backgroundColor='#88EE55'
+secondaryBackgroundColor='#E0F2C6'
 textColor='#262730'
 font='sans serif'
 " > ~/.streamlit/config.toml
