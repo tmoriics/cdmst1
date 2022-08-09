4a3c9c89541499153c7a8d3d2a64980892f75e9d
tmoriics <tmoriics-tky@umin.ac.jp>
2022-08-09 21:55:16 +0900

    Updated drop procedure.

--- home.py
+++ home.py
@@ -35,6 +35,7 @@
 # 2022-08-06T21:00 cdmst1 order of df columns
 # 2022-08-06T23:40 cdmst1 order of MEMO cannot be easily changed
 # 2022-08-09T18:40 cdmst1 changed indices calculation
+# 2022-08-09T21:42 cdmst1 changed drop 
 ########## WIP
 #     7/17 WIP アップロードこの方法ではcacheが働かない。memo機能も試したがでUploadのCacheは使わないでいくべき。
 #     8/ 5 Trying session state still
@@ -90,9 +91,9 @@ from st_aggrid.shared import GridUpdateMode, JsCode
 # Locale
 ###
 # locale.setlocale(locale.LC_ALL, 'en_US.UTF-8') # WORKED in mac for months. No good on Docker
-# locale.setlocale(locale.LC_ALL, 'C.UTF-8')  # Not work on mac local. probably works on all machines and all docker
+locale.setlocale(locale.LC_ALL, 'C.UTF-8')  # Not work on mac local. probably works on all machines and all docker
 # locale.setlocale(locale.LC_ALL, 'ja_JP.UTF-8') # No good on Heroku
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8') # for mac
+# locale.setlocale(locale.LC_ALL, 'en_US.UTF-8') # for mac
 
 ###
 # Timezone
@@ -1127,6 +1128,9 @@ def main():
     # print(urination_data_df['datetime_tmp_after_check'])
     # datetime tmp COPY
     urination_data_df['datetime'] = urination_data_df['datetime_tmp']
+    # erase line without datetime
+    urination_data_df_tmp = urination_data_df.copy()
+    urination_data_df = urination_data_df_tmp.dropna(subset=['datetime'])
 
     for index, row in urination_data_df.iterrows():
         if row['もれ無し'] == '無':
